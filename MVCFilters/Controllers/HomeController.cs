c11692aefddb3919e2836a9531a2c3d5519200a6
Guilherme Guizado <guilherme.guizado@mitt.ca>
2019-04-15 09:43:23 -0500

    Lecture about Authorization Filters

---
+++ MVCFilters/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFilters.Models.Filters;
+
+namespace MVCFilters.Controllers
+{
+    public class HomeController : Controller
+    {   
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [MVCFiltersAuthorization(Roles = "Admin")]
+        public ActionResult AdminsOnly()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Unauthorized()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCFilters/Models/Filters/MVCFiltersAuthorization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCFilters.Models.Filters
+{
+    public class MVCFiltersAuthorization : AuthorizeAttribute
+    {
+        // Override the default behavior when a user is not authenticated.
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                    { "controller", "Home" },
+                    { "action", "Unauthorized" }
+                });
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
